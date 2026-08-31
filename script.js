0f80589acdc391d4f62450eca606551dc07fb8e5
harshsixtus <harshmudgal77@gmail.com>
2026-09-01 00:06:34 +0530

    fixed the code on line 325 (added / predict in it )

--- script.js
+++ script.js
@@ -322,7 +322,7 @@ async function submitAssessment() {
   state.lastPayload = payload;
 
   try {
-    const response = await fetch(API_URL, {
+    const response = await fetch(`${API_URL}/predict`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
